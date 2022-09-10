421ea22b6844cab03d4ebe8a3dd82319ba2cb610
ametanishant <nishant.aqa@gmail.com>
2022-09-10 13:23:54 +0530

    git@github.com:ametanishant/restapi.git

--- main.go
+++ main.go
@@ -1,5 +1,64 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+//Author Struct
+type Author struct {
+	FirstName string `json:"firstanme"`
+	LastName  string `json:"lastname"`
+}
+
+//Book Struct(Model)
+type Book struct {
+	ID     string `json:"id"`
+	Isbn   string `json:"isbn"`
+	Title  string `json:"title"`
+	Author Author `json:"author"`
+}
+
+//Get All Books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 - Something bad happened!"))
+
+}
+
+//Get Single Book
+func getBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Create New Book
+func createBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Update Book
+func updateBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
+//Delete Book
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
-	println("test")
+	//Init Router
+	r := mux.NewRouter()
+
+	//Route handlers /Endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id]", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id]", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+
 }
